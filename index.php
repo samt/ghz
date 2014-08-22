df81f7e0c1aaa1d67fefebdca33b20c9c30f9d71
Sam Thompson <git@samt.us>
2014-08-22 08:43:55 -0700

    ga.js

--- index.php
+++ index.php
@@ -25,6 +25,20 @@ $app->savePostedUrl();
     <meta charset="utf-8">
     <title>GHz url shortener</title>
     <link rel="stylesheet" href="_assets/style.css">
+
+    <?php if (defined('GA_TRACKING')) : ?>
+      <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', '<?php echo GA_TRACKING; ?>', 'auto');
+        ga('send', 'pageview');
+
+      </script>
+    <?php endif; ?>
+
   </head>
   <body>
     <div class="main">
